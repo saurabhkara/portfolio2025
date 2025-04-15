ee2fed9910ed6b170f967dfc12fa038633dbd18d
wovvmac <wovvmac@wovvmacs-MacBook-Pro.local>
2025-04-15 19:23:51 +0530

    removed img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Saurabh's Portfolio",
-  description: "React Native developer",
+  description: "Mobile developer",
 };
 
 export default function RootLayout({
