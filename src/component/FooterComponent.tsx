44d734c307d56878581772c87d3df35f4457c679
wovvmac <wovvmac@wovvmacs-MacBook-Pro.local>
2025-04-17 12:23:28 +0530

    color and width fixed

--- src/component/FooterComponent.tsx
+++ src/component/FooterComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './footer.module.css'
 import SocialIconComponent from './SocialIconComponent';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function FooterComponent() {
 
@@ -8,13 +9,14 @@ export default function FooterComponent() {
 
     const copyClipBoard=()=>{
         navigator.clipboard.writeText('saurabhkumar679@gmail.com');
+        toast("Email copied to clipboard");
     }
     return (
         <div className={styles.footercomponent}>
             <div className={styles.mailContainer}>
-                <a href="mailto:saurabhkumar679@gmail.com"><img src="./mail.png" className={styles.mailIcon} /></a>
-                <p className={styles.mailid}>saurabhkumar679@gmail.com</p>
-                <div onClick={copyClipBoard}><img src="./copyIcon.png" className={styles.copyIcon} /></div>
+                <a href="mailto:saurabhkumar679@gmail.com" ><img src="./mail.png" className={styles.mailIcon} /></a>
+               <div> <p className={styles.mailid}>saurabhkumar679@gmail.com</p></div>
+                <div onClick={copyClipBoard} ><img src="./copyIcon.png" className={styles.copyIcon} /></div>
             </div>
             <div className={styles.socialComp}>
                 <p className={styles.findMessage}>You may also find me on these platforms!</p>
@@ -26,6 +28,7 @@ export default function FooterComponent() {
                 <img src="./heart.png" className={styles.copyrightHeart} />
                 <p className={styles.copyrighttext}>by Saurabh</p>
             </div>
+            <ToastContainer />
         </div>
     )
 }
