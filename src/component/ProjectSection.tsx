77a7d4a47c8333bae10d8e51b8fd0eac4b4aacb4
Saurabh <saurabkumar679@gmail..com>
2025-07-10 13:29:06 +0530

    projects

--- src/component/ProjectSection.tsx
+++ src/component/ProjectSection.tsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React from "react";
 import styles from "./projectSection.module.css";
-import ProjectCard from './ProjectCard';
+import ProjectCard from "./ProjectCard";
 
-const projectData=[
+const projectData = [
   {
-    id:'1p',
-    projectName:'Project Name',
-    projectDesc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.',
-    repoLink:'https://github.com/saurabhkara',
-    projectImg:'/Picture.png',
-    technologies:[
-      'JavaScript','Typescript','Kotlin','Android','Swift UI','IOS','Java','Node.js'
-    ]
+    id: "1p",
+    projectName: "Total Income & Expense AI agent",
+    projectDesc:
+      "Build an AI agent that will track all expenses and income. Click on repo link to know more",
+    repoLink: "https://github.com/saurabhkara/First-AI-Agent",
+    projectImg: "/Picture.png",
+    technologies: ["JavaScript", "Typescript", "Llama", "AI", "Node.js"],
   },
-]
+  {
+    id: "2p",
+    projectName: "AI Chat Clone",
+    projectDesc:
+      "Build an AI Chat clone of ChatGpt using React Native expo to learn and understand the AI integration. Check repo for more details",
+    repoLink: "https://github.com/saurabhkara/First-AI-Agent",
+    projectImg: "/Picture.png",
+    technologies: [
+      "JavaScript",
+      "Typescript",
+      "React Native Expo",
+      "Open AI",
+      "Node.js",
+      "express.js",
+      "Expo router",
+      "Expo API Routes",
+    ],
+  },
+];
 
 export default function ProjectSection() {
   return (
     <div className={styles.projectSection}>
-       {
-        projectData.map((item)=>{
-         return(<ProjectCard item={item} key={item.id}/>)
-        })
-       }
+      {projectData.map((item) => {
+        return <ProjectCard item={item} key={item.id} />;
+      })}
     </div>
-  )
+  );
 }
